52280e37ba5e3dfdff20ce5b01296b27d08c73ee
Chetan <chetankunarjain@gmail.com>
2023-08-28 18:15:31 +0530

    String

---
+++ _05_String/J_01_StringBasic.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class J_01_StringBasic {
+    public static void main(String args[]) {
+        // String a="chetan";
+        // String b="chetan";
+        // if(a==b){
+        // System.out.println("true");
+        // }
+        // ans= true
+
+        
+        // String a= new String("chetan");
+        // String b= new String("chetan");
+        // if(a==b){
+        // System.out.println("true");
+        // } else{
+        // System.out.println("false");
+        // }
+        // ans = false
+
+
+        // String a = new String("chetan");
+        // String b = new String("chetan");
+        // if (a.equals(b)) {
+        //     System.out.println("true");
+        // } else {
+        //     System.out.println("false");
+        // }
+        // // Ans=false
+       
+        //  String a="chetan";
+        // // String b="chetan";
+        // System.out.println(a); // Ans = chetan How did the value of a change 
+        //                                 //String are immutable
+        //      a ="ram" ;
+        // System.out.println(a); // Ans = ram
+    }
+}
